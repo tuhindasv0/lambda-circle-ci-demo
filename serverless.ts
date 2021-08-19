be853579d75ddf731164193b72d7f5a6af096f58
tuhindasv0 <tuhindasv0@gmail.com>
2021-08-19 08:02:39 +0530

    removing api gateway

--- serverless.ts
+++ serverless.ts
@@ -15,10 +15,6 @@ const serverlessConfiguration: AWS = {
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
